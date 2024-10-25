561e4ffa650c59de454f91d554f477b4e741b5f3
Nabil <nlamriben@vcinc.com>
2024-10-25 13:17:31 -0400

    Add project files.

---
+++ ProjectSourceFilesConfig.cmake
@@ -0,0 +1,47 @@
+
+#[[
+
+This is the User Adjustable CMake File for this project and should be updated when new source files are added or old ones removed.
+
+]]
+
+set ( PROJECTSOURCES
+#list the source files to be used here:
+source/CAN/can.cpp
+source/CAN/J1939/J1939_FEE5_HOURS.cpp
+source/Database/database.cpp
+source/database/unitconv.cpp
+source/database/serialinterpolate.cpp
+source/database/systemoptions.cpp
+source/IO/iouser.cpp
+source/Language/language.cpp
+source/NVSettings/settings.cpp
+source/System/eventhandler.cpp
+source/System/sdk_init.cpp
+source/System/lighting.cpp
+source/System/ipAddress.cpp
+source/UART/uart.cpp
+source/UART/NMEA0183/extractField.cpp
+source/UART/NMEA0183/VTG.cpp
+source/UART/uart_util.cpp
+source/UI/Menus/menu_diagnostic.cpp
+source/UI/Menus/menu_home.cpp
+source/UI/Menus/menu_language.cpp
+source/UI/Menus/menu_settings.cpp
+source/UI/Menus/menu_units.cpp
+source/UI/Screens/screen1.cpp
+source/UI/Screens/screen2.cpp
+source/UI/Screens/screen3.cpp
+source/UI/Screens/screen4.cpp
+source/UI/Screens/screen_passcode.cpp
+source/UI/Screens/screen_about.cpp
+source/UI/Screens/screen_debug.cpp
+source/UI/screens.cpp
+source/UI/ttfonts.cpp
+source/UI/buttonbar.cpp
+source/UI/menu.cpp
+source/UI/keyboard.cpp
+source/UI/graphs.cpp
+source/data.cpp
+source/main.cpp
+)
